937ea76fd7db3e5ec5be4f0b1e677ab2af6e2a5e
Paweł Ruciński <pawel.rucinski@cybercom.com>
2018-03-02 22:25:26 +0100

    removed empty line

--- src/app/bitcoin/bitcoin.module.ts
+++ src/app/bitcoin/bitcoin.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ChartsModule } from 'ng2-charts';
-import { HttpClient } from '@angular/common/http';
-
 
 import { BitcoinComponent } from './bitcoin.component';
 import { BitcoinService } from './bitcoin.service';
@@ -15,6 +13,6 @@ import { BitcoinService } from './bitcoin.service';
     ChartsModule
   ],
   declarations: [BitcoinComponent],
-  providers: [BitcoinService, HttpClient]
+  providers: [BitcoinService]
 })
 export class BitcoinModule { }
